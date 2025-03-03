39f4199aa6f92d9c04f4c5341d23624ab7e4aac2
Andrea <a.kost@hbk-bs.de>
2025-03-03 14:05:04 +0100

    proportion

--- perspectives/proportion/index.js
+++ perspectives/proportion/index.js
@@ -1,54 +1,34 @@
-let ball1X; // Position der ersten Kugel (von links nach rechts)
-let ball2X; // Position der zweiten Kugel (von rechts nach links)
-let ballY1; // Y-Position der ersten Kugel (grün)
-let ballY2; // Y-Position der zweiten Kugel (pink)
-let ballSize = 50; // Größe der Kugeln
-let target1; //Zielposition Kugel 1
-let target2; //Zielposition Kugel 2
-
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
 
-  // Start- und Zielpositionen der Kugeln
-  ball1X = 0; 
-  target1 = width;
+	background("DarkSalmon");
 
-  ball2X = width;
-  target2 = 0;
+	fill("Bisque");
+	ellipse(200, 200, 270, 300);
 
-  ballY1 = height / 3; // Y-Position der grünen Kugel (etwas höher als die pinke)
-  ballY2 = height / 2; // Y-Position der pinken Kugel (in der Mitte des Canvas)
+	fill("White");
+	ellipse(150, 200, 70, 40);
+	fill("LightBlue");
+	ellipse(150, 200, 40, 40);
+	fill("Black");
+	ellipse(150, 200, 20, 20);
+
+	fill("White");
+	ellipse(300, 210, 150, 100);
+	fill("LightBlue");
+	ellipse(300, 210, 100, 100);
+	fill("Black");
+	ellipse(300, 210, 50, 50);
+
+	fill("Bisque");
+	triangle(200, 200, 215, 250, 185, 250);
+
+	stroke("LightCoral");
+	strokeWeight(5);
+
+	line(150, 300, 250, 300);
 }
 
-function draw() {
-  background(255);
-  
-  // Berechnung easing für beide Kugeln
-  let easeFactor1 = 0.05; //Geschmeidigkeit
-  ball1X += (target1 - ball1X) * easeFactor1;//Langsames Annähern an Ziel
-
-  let easeFactor2 = 0.05;
-  ball2X += (target2 - ball2X)* easeFactor2; 
-
-
-//Sobald Ziel erreicht, wechseln Kugeln Richtung
-  if (abs(ball1X - target1) < 1) {
-    target1 = target1 === width ? 0 : width;
-  }
-
-  if (abs(ball2X - target2) < 1) {
-    target2 = target2 === 0 ? width : 0;
-  }
-  // Zeichnen der ersten Kugel (grün, etwas höher)
-  fill(0, 255, 0); // Grün
-  noStroke();
-  ellipse(ball1X, ballY1, ballSize, ballSize);
-
-  // Zeichnen der zweiten Kugel (pink)
-  fill(255, 0, 255); // Pink
-  noStroke();
-  ellipse(ball2X, ballY2, ballSize, ballSize);
-}
 
 
  
