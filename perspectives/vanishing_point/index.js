28b73568ef643b1b298b286224d8cfdcbd54e8bc
Andrea <a.kost@hbk-bs.de>
2025-03-03 14:06:23 +0100

    vanishing

--- perspectives/vanishing_point/index.js
+++ perspectives/vanishing_point/index.js
@@ -1,53 +1,43 @@
-let ball1X; // Position der ersten Kugel (von links nach rechts)
-let ball2X; // Position der zweiten Kugel (von rechts nach links)
-let ballY1; // Y-Position der ersten Kugel (grün)
-let ballY2; // Y-Position der zweiten Kugel (pink)
-let ballSize = 50; // Größe der Kugeln
-let target1; //Zielposition Kugel 1
-let target2; //Zielposition Kugel 2
-
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
+	background("lightblue");
 
-  // Start- und Zielpositionen der Kugeln
-  ball1X = 0; 
-  target1 = width;
+	let houseX = 50;
+	let houseY = 250;
+	let houseWidth = 100;
+	let houseHeight = 100;
 
-  ball2X = width;
-  target2 = 0;
+	let roofHeight = 50;
 
-  ballY1 = height / 3; // Y-Position der grünen Kugel (etwas höher als die pinke)
-  ballY2 = height / 2; // Y-Position der pinken Kugel (in der Mitte des Canvas)
-}
+	let vanishingPointX = 350;
+	let vanishingPointY = 75;
 
-function draw() {
-  background(255);
-  
-  // Berechnung easing für beide Kugeln
-  let easeFactor1 = 0.05; //Geschmeidigkeit
-  ball1X += (target1 - ball1X) * easeFactor1;//Langsames Annähern an Ziel
+	rect(houseX, houseY, houseWidth, houseHeight);
 
-  let easeFactor2 = 0.05;
-  ball2X += (target2 - ball2X)* easeFactor2; 
+	triangle(
+		houseX,
+		houseY,
+		houseX + houseWidth / 2,
+		houseY - roofHeight,
+		houseX + houseWidth,
+		houseY
+	);
 
+	line(
+		houseX + houseWidth,
+		houseY + houseHeight,
+		vanishingPointX,
+		vanishingPointY
+	);
+	line(houseX + houseWidth, houseY, vanishingPointX, vanishingPointY);
+	line(
+		houseX + houseWidth / 2,
+		houseY - roofHeight,
+		vanishingPointX,
+		vanishingPointY
+	);
 
-//Sobald Ziel erreicht, wechseln Kugeln Richtung
-  if (abs(ball1X - target1) < 1) {
-    target1 = target1 === width ? 0 : width;
-  }
-
-  if (abs(ball2X - target2) < 1) {
-    target2 = target2 === 0 ? width : 0;
-  }
-  // Zeichnen der ersten Kugel (grün, etwas höher)
-  fill(0, 255, 0); // Grün
-  noStroke();
-  ellipse(ball1X, ballY1, ballSize, ballSize);
-
-  // Zeichnen der zweiten Kugel (pink)
-  fill(255, 0, 255); // Pink
-  noStroke();
-  ellipse(ball2X, ballY2, ballSize, ballSize);
+	saveCanvas("mysketch", "png");
 }
 
 
