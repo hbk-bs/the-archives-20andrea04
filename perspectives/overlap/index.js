40be7f0f351dddffa20f27dda055ce9855f4183a
Andrea <a.kost@hbk-bs.de>
2025-03-03 14:03:34 +0100

    overlap

--- perspectives/overlap/index.js
+++ perspectives/overlap/index.js
@@ -1,53 +1,30 @@
-let ball1X; // Position der ersten Kugel (von links nach rechts)
-let ball2X; // Position der zweiten Kugel (von rechts nach links)
-let ballY1; // Y-Position der ersten Kugel (grün)
-let ballY2; // Y-Position der zweiten Kugel (pink)
-let ballSize = 50; // Größe der Kugeln
-let target1; //Zielposition Kugel 1
-let target2; //Zielposition Kugel 2
+const x = (n) => width * n;
+const y = (n) => height * n;
+const s = (n) => (height > width ? height * n : width * n);
 
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
+	angleMode(DEGREES);
+	background("DarkSeaGreen");
 
-  // Start- und Zielpositionen der Kugeln
-  ball1X = 0; 
-  target1 = width;
+	push();
+	translate(width / 2, height / 2);
+	rotate(90);
+	const d = s(0.22); //d bestimmt Größe Kreis
+	pop();
 
-  ball2X = width;
-  target2 = 0;
+	fill("Pink");
+	circle(100, 100, d / 2);
+	fill("PeachPuff");
+	ellipse(130, 130, d / 1.5);
+	fill("MediumAquaMarine");
+	ellipse(160, 160, d / 1.25);
+	fill("LightBlue");
+	ellipse(200, 200, d);
+	fill("Lavender");
+	ellipse(240, 240, d * 1.25);
 
-  ballY1 = height / 3; // Y-Position der grünen Kugel (etwas höher als die pinke)
-  ballY2 = height / 2; // Y-Position der pinken Kugel (in der Mitte des Canvas)
-}
-
-function draw() {
-  background(255);
-  
-  // Berechnung easing für beide Kugeln
-  let easeFactor1 = 0.05; //Geschmeidigkeit
-  ball1X += (target1 - ball1X) * easeFactor1;//Langsames Annähern an Ziel
-
-  let easeFactor2 = 0.05;
-  ball2X += (target2 - ball2X)* easeFactor2; 
-
-
-//Sobald Ziel erreicht, wechseln Kugeln Richtung
-  if (abs(ball1X - target1) < 1) {
-    target1 = target1 === width ? 0 : width;
-  }
-
-  if (abs(ball2X - target2) < 1) {
-    target2 = target2 === 0 ? width : 0;
-  }
-  // Zeichnen der ersten Kugel (grün, etwas höher)
-  fill(0, 255, 0); // Grün
-  noStroke();
-  ellipse(ball1X, ballY1, ballSize, ballSize);
-
-  // Zeichnen der zweiten Kugel (pink)
-  fill(255, 0, 255); // Pink
-  noStroke();
-  ellipse(ball2X, ballY2, ballSize, ballSize);
+	saveCanvas("mysketch", "png");
 }
 
 
